4bf0b789182a8f7c583722544cb8e4fee634cb06
Angelo1793 <angelo1793@hotmail.com>
2017-10-14 10:25:19 -0500

    Avanzando la vista de destino
    - Modificando el objeto lista en detinos

--- Aplicacion_Movil/Toupaq_application/app/src/main/java/com/example/angelo/toupaq_application/Destinos.java
+++ Aplicacion_Movil/Toupaq_application/app/src/main/java/com/example/angelo/toupaq_application/Destinos.java
@@ -2,6 +2,7 @@ package com.example.angelo.toupaq_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,11 +14,13 @@ public class Destinos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destinos);
-
+        //Crenado lista,arraylist,adaptador
         ListView lista;
         ArrayList<String> destinos;
         ArrayAdapter<String> adaptador;
-
+        //creando lista y  enlazandola con el xml
+        lista = (ListView)findViewById(R.id.listDestinos);
+        //creacion de arrar list de destinos
         destinos= new ArrayList<String>();
         destinos.add("Arequipa");
         destinos.add("Lima");
@@ -25,8 +28,16 @@ public class Destinos extends AppCompatActivity {
         destinos.add("Loreto");
         destinos.add("Ica");
         destinos.add("Lambayeque");
+        //Creando adaptador
         adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,destinos);
-        lista = (ListView)findViewById(R.id.listDestinos);
+
         lista.setAdapter(adaptador);
+
+        lista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
