e67ad5824c54648c5e079a68aae7cae266713449
Angelo1793 <angelo1793@hotmail.com>
2017-10-13 20:30:37 -0500

    Modificacion de lista
    - Modificacion de los valores de la lista de los destinos , estableciendo valores predeterminados

--- Aplicacion_Movil/Toupaq_application/app/src/main/java/com/example/angelo/toupaq_application/Destinos.java
+++ Aplicacion_Movil/Toupaq_application/app/src/main/java/com/example/angelo/toupaq_application/Destinos.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class Destinos extends AppCompatActivity {
 
     @Override
@@ -13,12 +15,18 @@ public class Destinos extends AppCompatActivity {
         setContentView(R.layout.activity_destinos);
 
         ListView lista;
+        ArrayList<String> destinos;
         ArrayAdapter<String> adaptador;
 
+        destinos= new ArrayList<String>();
+        destinos.add("Arequipa");
+        destinos.add("Lima");
+        destinos.add("Cuzco");
+        destinos.add("Loreto");
+        destinos.add("Ica");
+        destinos.add("Lambayeque");
+        adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,destinos);
         lista = (ListView)findViewById(R.id.listDestinos);
-
-        adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
-
         lista.setAdapter(adaptador);
     }
 }
